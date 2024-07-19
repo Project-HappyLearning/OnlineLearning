c02ceb1a5cd61b2b64e01d0af9fdeacbc6bfe0d8
Trung Nguyen <nguyenpthe172112@fpt.edu.vn>
2024-07-19 22:28:38 +0700

    admin

---
+++ src/java/controller/admin/matchgame/ChoiceClassToAddGame.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.admin.matchgame;
+
+import controller.auth.BaseAuthorizationController;
+import dal.ClassDBContext;
+import dal.CourseDBContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import model.Account;
+import model.Course;
+import model.Class;
+
+@WebServlet("/admin/ChoiceClassToAddGameImage")
+public class ChoiceClassToAddGame extends BaseAuthorizationController {
+
+    @Override
+    protected void processPost(HttpServletRequest req, HttpServletResponse resp, Account account) throws ServletException, IOException {
+        int class_id = Integer.parseInt(req.getParameter("grade"));
+        CourseDBContext cDB = new CourseDBContext();
+        ArrayList<Course> courses = cDB.listCoursebyClassId(class_id);
+        req.getSession().setAttribute("class_id_game", class_id);
+        req.getSession().setAttribute("coursesToAddGame", courses);
+        req.getRequestDispatcher("./choiceGameLession.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void processGet(HttpServletRequest req, HttpServletResponse resp, Account account) throws ServletException, IOException {
+        ClassDBContext clDB = new ClassDBContext();
+        ArrayList<model.Class> classes = clDB.list();
+        req.getSession().setAttribute("classesToAddGame", classes);
+        req.getRequestDispatcher("./choiceGameLession.jsp").forward(req, resp);
+    }
+
+}
